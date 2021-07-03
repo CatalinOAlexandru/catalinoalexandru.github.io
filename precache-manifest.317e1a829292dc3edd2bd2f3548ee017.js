80cd4c419d25e74adc48ec595ce91e4f00e9e502
Catalin Alexandru <alex97catalin@yahoo.com>
2021-07-03 18:41:36 +0100

    initial

---
+++ precache-manifest.317e1a829292dc3edd2bd2f3548ee017.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9bbaeacfbf34e79c59ca8c33c4c99a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf576bd40f6fd1530674",
+    "url": "/static/css/3.bc85b1b3.chunk.css"
+  },
+  {
+    "revision": "168309654c178f7fafab",
+    "url": "/static/css/4.5c57288c.chunk.css"
+  },
+  {
+    "revision": "7b238876a97adf6788ce",
+    "url": "/static/css/main.d8761c1d.chunk.css"
+  },
+  {
+    "revision": "a446556ff1d32d6feca6",
+    "url": "/static/js/2.0aa8bae2.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.0aa8bae2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf576bd40f6fd1530674",
+    "url": "/static/js/3.d5d477d4.chunk.js"
+  },
+  {
+    "revision": "168309654c178f7fafab",
+    "url": "/static/js/4.fbdfebcc.chunk.js"
+  },
+  {
+    "revision": "7b238876a97adf6788ce",
+    "url": "/static/js/main.41b184ad.chunk.js"
+  },
+  {
+    "revision": "ebb70e5b19f41dc1f0e1",
+    "url": "/static/js/runtime-main.563fc749.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "0c3c2f9fa4440be6cd77b0d23e762a1c",
+    "url": "/static/media/adamarobotics.0c3c2f9f.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "cb4414c2ae58e5910b6ae4bb8f570d9f",
+    "url": "/static/media/kpmg.cb4414c2.jpg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "c49bf4d6ea009e279c9faa6d41d4c6b1",
+    "url": "/static/media/nvidia.c49bf4d6.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "b0af8122d7727dbb4310ecfaf50b4a1d",
+    "url": "/static/media/qmairs.b0af8122.png"
+  },
+  {
+    "revision": "ce1d3bfc54af42a702ec9021e6603f66",
+    "url": "/static/media/qmul.ce1d3bfc.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "13bfe9bf7442819622baaabd0d3cafb4",
+    "url": "/static/media/ubisoft.13bfe9bf.png"
+  },
+  {
+    "revision": "bb58473fd9ec42b31bfca7d00fa8dc51",
+    "url": "/static/media/ucl2.bb58473f.png"
+  }
+]);
